7bc2233f36478572f046a0ad5b516fe491ac5871
Caio Vinicius De Lima <32105582+caiovlima@users.noreply.github.com>
2019-08-12 22:57:11 -0300

    update 7181
    create new repository for 7181 - ComecandoComAngular

---
+++ 7181-ComecandoComAngular/todo/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Todo } from './../shared/models/todo.model';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+
+  constructor( private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    });
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao mercado', true));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
+}
